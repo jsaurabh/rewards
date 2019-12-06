7b45c3e439272706407bd75455e49bed8f40df52
jsaurabh <j.saur95@gmail.com>
2019-12-06 09:30:18 -0500

    debug allowed hosts fix

--- rewards/settings.py
+++ rewards/settings.py
@@ -29,8 +29,8 @@ import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = True
+#DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
